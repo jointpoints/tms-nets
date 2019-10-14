67fe4b7ac269b8b5ac81bc6ad1a396bd92acb72d
jointpoints <55635871+jointpoints@users.noreply.github.com>
2019-10-14 21:07:26 +0300

    Отредактированные исходники Нидеррайтера
    Оригинал: https://people.sc.fsu.edu/~jburkardt/cpp_src/niederreiter2/niederreiter2.html

---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+#include "niederreiter2.h"
+
+int main();
+void test01();
+
+
+
+int main (void)
+{
+	timestamp();
+	cout << "\n";
+	cout << "NIEDERREITER2_PRB\n";
+	cout << "	C++ version\n";
+	cout << "Test the NIEDERREITER2 library.\n";
+
+	test01();
+	
+	cout << "\n";
+	cout << "NIEDERREITER2_PRB\n";
+	cout << "	Normal end of execution.\n";
+
+	cout << "\n";
+	timestamp();
+
+	return 0;
+}
+
+
+
+void test01 (void)
+{
+#	define DIM 2
+	int dim_num;
+	int i;
+	int j;
+	double r[DIM];
+	int seed;
+	
+	seed = 0;
+	
+	for(i = 0; i < 16; ++i)
+	{
+		niederreiter2(DIM, &seed, r);
+		
+		for (j = 0; j < DIM; j++)
+		{
+			cout << setw(10) << r[j] << " ";
+		}
+		cout << "\n";
+	}
+	
+	return;
+#	undef DIM_MAX
+}
