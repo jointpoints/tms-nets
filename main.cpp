46ea3790f1f6d8b7bddbd9fcd87ea76325e265db
buralex-git <buralex.gm@gmail.com>
2019-11-16 18:34:59 +0300

    Класс генератор стал шаблонным
    Теперь наш класс зависит от двух шаблонных параметров:
    1. Тип, который используется для хранения целочисленных масштабированных в 2**NBITS раз сеток;
    2. NBITS, который отныне может быть любым ненулевым числом разрядов, помещающихся в первый указанный параметр шаблона.
    
    Из-за преобразования в шаблонный класс, вся реализация переехала в заголовочный файл, ввиду чего niederreiter2.cpp был удалён за ненадобностью.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
-#include <iomanip>
-#include <cmath>
 
 #include "niederreiter2.hpp"
 
@@ -16,29 +14,29 @@ int main(void)
 	int dim     = DEFAULT_DIM_VALUE;
 	int expo    = DEFAULT_EXPO_VALUE;
 	
-	//Create generator of (t,s) sequences with s == dim and consequently implicitly defined t.
-	Nied2Generator generator(dim);
-	
-	//Generate 200 (not 1 << 200!!) points of (t,s)-sequence, beginning with 10 sequence number
-	std::vector<Nied2Generator::RealPoint> points = generator.get_points_real(10, 200);
+	using GeneratorType = Nied2Generator<uint64_t, 64>;
 
-	//Load another 200 (not 1 << 200!!!) points of (t,s)-sequence, beginning with 100 sequence number into points-vector
+	//Create generator of (t,s) sequences with s == dim and consequently implicitly defined t.
+	GeneratorType generator(dim);
+	
+	//Generate 20 (not 1 << 20!!) points of (t,s)-sequence, beginning with 10 sequence number
+	std::vector<GeneratorType::RealPoint> points = generator.get_points_real(0, 20);
+
+	//Load another 20 (not 1 << 20!!!) points of (t,s)-sequence, beginning with 100 sequence number into points-vector
 	generator.load_points_real(points, 100);
 
 
-	std::vector<Nied2Generator::IntPoint> points_int;
+	std::vector<GeneratorType::IntPoint> points_int;
 	//ERROR: because points_int are empty
-	//generator.load_points_int(points_int, 10);
+	//	generator.load_points_int(points_int, 10);
 
-	points_int = std::vector<Nied2Generator::IntPoint>(20);
+	points_int = std::vector<GeneratorType::IntPoint>(20);
 	//ERROR: because points_int containing empty points
-	//generator.load_points_int(points_int, 10);
+	//	generator.load_points_int(points_int, 10);
 
-	points_int = std::vector<Nied2Generator::IntPoint>(20, Nied2Generator::IntPoint(dim));
+	points_int = std::vector<GeneratorType::IntPoint>(20, GeneratorType::IntPoint(dim));
 	//OK: 20 consequent points of (t,s)-sequence will be loaded into points_int
 	generator.load_points_int(points_int, 10);
-	
-	//kak-to tak
 	
 	return 0;
 }
