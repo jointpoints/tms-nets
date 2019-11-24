81508995c59e9d23b81f1f72862ace37163dbb62
buralex-git <buralex.gm@gmail.com>
2019-11-24 18:54:06 +0300

    Обновил наименования в test/main.cpp
    Из-за использования неактуальных имён стандартная сборка падала. Теперь этого не происходит.

--- test/main.cpp
+++ test/main.cpp
@@ -23,20 +23,20 @@ int main(void)
 	std::vector<uint32_t> degrees1 = {3, 6, 5, 5};
 	std::vector<uint32_t> degrees2 = {3, 6, 5, 5, 3, 4, 3, 2};
 	
-	
+	using GeneratorType = sequences::Niederreiter<uint64_t, 63>;
 	
 	/*                       15
 	 * 4-dimensional set of 2   points with the best defect possible
 	 * This should be a (t, m, s)-net
 	 */
-	Nied2Generator<uint64_t, 63> generator(4);
+	GeneratorType generator(4);
 	nied2_check_definition(&generator, 1ULL << 15);
 	
 	/*                       15
 	 * 4-dimensional set of 2   points with the manually specified degrees of polynoms
 	 * This should be a (t, m, s)-net
 	 */
-	generator = Nied2Generator<uint64_t, 63>(degrees1);
+	generator = GeneratorType(degrees1);
 	nied2_check_definition(&generator, 1ULL << 15);
 	
 	
@@ -45,21 +45,21 @@ int main(void)
 	 * 8-dimensional set of 2   points with the manually specified degrees of polynoms
 	 * This should NOT be a (t, m, s)-net (due to very high degrees)
 	 */
-	generator = Nied2Generator<uint64_t, 63>(degrees2);
+	generator = GeneratorType(degrees2);
 	nied2_check_definition(&generator, 1ULL << 15);
 	
 	/*                       15
 	 * 4-dimensional set of 2   + 1 points with the manually specified degrees of polynoms
 	 * This should NOT be a (t, m, s)-net (due to incorrect amount of points)
 	 */
-	generator = Nied2Generator<uint64_t, 63>(degrees1);
+	generator = GeneratorType(degrees1);
 	nied2_check_definition(&generator, (1ULL << 15) + 1);
 	
 	/*                       15
 	 * 4-dimensional set of 2   + 1 points with the best defect possible
 	 * This should NOT be a (t, m, s)-net (due to incorrect amount of points)
 	 */
-	generator = Nied2Generator<uint64_t, 63>(4);
+	generator = GeneratorType(4);
 	nied2_check_definition(&generator, (1ULL << 15) + 1);
 	
 	return 0;
