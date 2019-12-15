a7a35b6a8569fe5dfdd1439985970293413c8744
jointpoints <jointpoints@users.noreply.github.com>
2019-12-15 21:25:32 +0300

    Добавлен автоматический тестер
    Авторы: Михаил Лазуков (КМБО-03-16) и Аветик Варданян (КМБО-03-16).

--- test/main.cpp
+++ test/main.cpp
@@ -2,16 +2,17 @@
  *	\file main.cpp
  *
  *	\author
- *		Andrew Yeliseyev (Russian Technological University, KMBO-03-16, Russia, 2019)
+ *		Michail Lazukov (Russian Technological University, KMBO-03-16, Russia, 2019),
+ *		Avetik Vardanyan (Russian Technological University, KMBO-03-16, Russia, 2019)
  */
 
 
 
 /*
- * Uncomment the following line to enable verbosity
+ * Use the following line to control verbosity
  * Possible levels of verbosity: 1, 2, 3, 4
  */
-//#define VERBOSITY_LEVEL 1
+#define VERBOSITY_LEVEL 2
 
 
 
@@ -21,37 +22,25 @@
 
 int main(void)
 {
-	std::vector<uint32_t> degrees1 = {3, 6, 5, 5, 3};
-	std::vector<uint32_t> degrees2 = {3, 6, 5, 5, 3, 4};
+	using MyGeneratorType = sequences::Niederreiter<uint32_t, 32>;
 	
-	using GeneratorType = sequences::Niederreiter<uint32_t, 32>;
-	
-	/*                       15
-	 * 4-dimensional set of 2   points with the best defect possible
-	 * This should be a (t, m, s)-net
-	 */
-	GeneratorType generator(4);
-	niederreiter_check_uniqueness(&generator, 15);
-	niederreiter_check_definition(&generator, 15);
-	std::cout << '\n';
-	
-	/*                       20
-	 * 5-dimensional set of 2   points with the manually specified degrees of polynoms
-	 * This should be a (t, m, s)-net
-	 */
-	generator = GeneratorType(degrees1);
-	niederreiter_check_uniqueness(&generator, 20);
-	niederreiter_check_definition(&generator, 20);
-	std::cout << '\n';
-	
-	/*                       15
-	 * 6-dimensional set of 2   points with the manually specified degrees of polynoms
-	 * This should NOT be a (t, m, s)-net (due to very high degrees)
-	 */
-	generator = GeneratorType(degrees2);
-	niederreiter_check_uniqueness(&generator, 15);
-	niederreiter_check_definition(&generator, 15);
-	std::cout << '\n';
-	
+	int cur   = 0;
+	int s     = 0;
+	int m     = 0;
+	int s_max = 7;
+	int m_max = 16;
+	srand(time(0));
+	while (cur < 100)
+	{
+	    cur += 1;
+	    std::cout << "TEST #" << cur << "/100\n";
+		s = rand() % s_max + 1;
+	    MyGeneratorType my_generator(s);
+        m = rand() % (m_max - my_generator.get_t() + 1) + my_generator.get_t();
+		niederreiter_check_definition(&my_generator, m);
+		niederreiter_check_uniqueness(&my_generator, m);
+		std::cout << "----------------------------------\n\n";
+	}
+
 	return 0;
 }
