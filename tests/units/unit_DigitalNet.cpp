f376fd1f145232469694c0d2e7d2da6ad4983483
jointpoints <jointpoints@ya.ru>
2021-05-02 18:27:34 +0300

    Update of unit test for tms::DigitalNet
    1. Unit tests for tms::DigitalNet class have been updated.

--- tests/units/unit_DigitalNet.cpp
+++ tests/units/unit_DigitalNet.cpp
@@ -30,11 +30,53 @@ TEST_CASE("Validation of DigitalNet class", "[nets][DigitalNet]")
 		REQUIRE( nondeg_net.get_s() == 3 );
 	}
 
-	SECTION("Construction of 3-dimensional point")
+	SECTION("Check generating matrices")
 	{
-		tms::Point point = nondeg_net.generate_point_classical(5);
-		REQUIRE( point[0] == Catch::Approx(0.25).margin(0.0001) );
-		REQUIRE( point[1] == Catch::Approx(0.625).margin(0.0001) );
-		REQUIRE( point[2] == Catch::Approx(0.125).margin(0.0001) );
+		REQUIRE( nondeg_net.get_generating_matrix(0) == generating_matrices[0] );
+		REQUIRE( nondeg_net.get_generating_matrix(1) == generating_matrices[1] );
+		REQUIRE( nondeg_net.get_generating_matrix(2) == generating_matrices[2] );
+	}
+
+	SECTION("Check direction numbers")
+	{
+		tms::DirNum direction_numbers = nondeg_net.get_direction_numbers(0);
+		REQUIRE( direction_numbers[0] == 6 );
+		REQUIRE( direction_numbers[1] == 1 );
+		REQUIRE( direction_numbers[2] == 4 );
+		direction_numbers = nondeg_net.get_direction_numbers(1);
+		REQUIRE( direction_numbers[0] == 1 );
+		REQUIRE( direction_numbers[1] == 2 );
+		REQUIRE( direction_numbers[2] == 4 );
+		direction_numbers = nondeg_net.get_direction_numbers(2);
+		REQUIRE( direction_numbers[0] == 6 );
+		REQUIRE( direction_numbers[1] == 5 );
+		REQUIRE( direction_numbers[2] == 7 );
+	}
+
+	SECTION("Construction of 3-dimensional point (classical enumeration of points)")
+	{
+		// classical representation of index 4 is (0 0 1)^T
+		tms::Point point = nondeg_net.generate_point_classical(4);
+		REQUIRE( point[0] == Catch::Approx(0.5).margin(0.0001) );
+		REQUIRE( point[1] == Catch::Approx(0.5).margin(0.0001) );
+		REQUIRE( point[2] == Catch::Approx(0.875).margin(0.0001) );
+	}
+
+	SECTION("Construction of 3-dimensional point (Gray code enumeration of points)")
+	{
+		// Gray representation of index 4 is (0 1 1)^T
+		tms::Point point = nondeg_net.generate_point(4);
+		REQUIRE( point[0] == Catch::Approx(0.625).margin(0.0001) );
+		REQUIRE( point[1] == Catch::Approx(0.75).margin(0.0001) );
+		REQUIRE( point[2] == Catch::Approx(0.25).margin(0.0001) );
+	}
+
+	SECTION("Construction of 3-dimensional point (Gray code enumeration of points, integer point)")
+	{
+		// Gray representation of index 4 is (0 1 1)^T
+		tms::IntPoint point = nondeg_net.generate_point_int(4);
+		REQUIRE( point[0] == 5 );
+		REQUIRE( point[1] == 6 );
+		REQUIRE( point[2] == 2 );
 	}
 }
